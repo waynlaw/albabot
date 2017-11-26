546b65c316bf8b633ff3d7055b6923e91d6e9412
Wayne <waynejo1024@gmail.com>
2017-11-26 13:57:36 +0900

    Add selling tests

--- src/test/scala/StrategistSuite.scala
+++ src/test/scala/StrategistSuite.scala
@@ -88,4 +88,76 @@ class StrategistSuite extends FunSuite {
         val (newState, _) = strategist.compute(state, Event.BuyingOrderConfirmed(timestamp, "id123"), timestamp + 100)
         assert(newState.cryptoCurrency == List(CryptoCurrencyInfo(RealNumber(200), 400, CryptoCurrencyState.WaitingForBuying("id123", Nil))))
     }
+
+    test("구매 체결 테스트") {
+        val strategist = new Strategist(decisionMaker, 20)
+
+        val currencyHistory = for {
+            i <- 0 to Strategist.HISTORY_MINIMUM_FOR_TRADING.toInt
+        } yield CurrencyInfo(201, 200 * i)
+
+        val timestamp = 300
+
+        val beginKrw = 10000000
+        val state: StrategistModel = StrategistModel(
+            krw = beginKrw,
+            state = State.Trading,
+            history = currencyHistory.toArray,
+            cryptoCurrency = List(CryptoCurrencyInfo(RealNumber(200), 400, CryptoCurrencyState.WaitingForBuying("id123", Nil)))
+        )
+
+
+
+        val (newState, action) = strategist.compute(state, Event.ReceiveOrderInfo("id123", List(Event.ConfirmOrderInfo("t1", RealNumber(50), 0, RealNumber(1)))), timestamp + 100)
+        assert(action.contains(Action.RequestTradingInfo("id123", isBuying = true)))
+        assert(newState.cryptoCurrency.contains(CryptoCurrencyInfo(RealNumber(149), 400, CryptoCurrencyState.WaitingForBuying("id123", List("t1")))))
+        assert(newState.cryptoCurrency.contains(CryptoCurrencyInfo(RealNumber(50), 400, CryptoCurrencyState.Nothing)))
+        assert(newState.krw == beginKrw)
+
+        val (newState2, action2) = strategist.compute(newState, Event.ReceiveOrderInfo("id123", List(Event.ConfirmOrderInfo("t2", RealNumber(50), 10, RealNumber(1)))), timestamp + 100)
+        assert(action2.contains(Action.RequestTradingInfo("id123", isBuying = true)))
+        assert(newState2.cryptoCurrency.contains(CryptoCurrencyInfo(RealNumber(98), 400, CryptoCurrencyState.WaitingForBuying("id123", List("t2", "t1")))))
+        assert(newState2.cryptoCurrency.contains(CryptoCurrencyInfo(RealNumber(100), 400, CryptoCurrencyState.Nothing)))
+        assert(newState2.krw == beginKrw + 50 * 10)
+
+        val (newState3, action3) = strategist.compute(newState2, Event.ReceiveOrderInfo("id123", List(Event.ConfirmOrderInfo("t3", RealNumber(97), 0, RealNumber(1)))), timestamp + 100)
+        assert(newState3.cryptoCurrency.contains(CryptoCurrencyInfo(RealNumber(197), 400, CryptoCurrencyState.Nothing)))
+
+        val (_, action4) = strategist.compute(newState3, Event.ReceiveOrderInfo("id123", List(Event.ConfirmOrderInfo("t3", RealNumber(97), 0, RealNumber(1)))), timestamp + 100)
+        assert(!action4.contains(Action.RequestTradingInfo("id123", isBuying = true)))
+    }
+
+    test("판매 주문 반영 테스트") {
+        val strategist = new Strategist(decisionMaker, 20)
+
+        val currencyHistory = for {
+            i <- 0 to Strategist.HISTORY_MINIMUM_FOR_TRADING.toInt
+        } yield CurrencyInfo(60001 - i, 200 * i)
+
+        val timestamp = 1300
+
+        val state: StrategistModel = StrategistModel(
+            krw = 1,
+            state = State.Trading,
+            history = currencyHistory.toArray,
+            cryptoCurrency = List(CryptoCurrencyInfo(RealNumber(200), 400, CryptoCurrencyState.Nothing))
+        )
+
+        val (newState, action) = strategist.compute(state, Event.Tick, timestamp)
+
+        assert(action.contains(Action.RequestSell(RealNumber(200), 59940, timestamp)))
+        assert(newState.cryptoCurrency == List(CryptoCurrencyInfo(RealNumber(200), 59940, CryptoCurrencyState.TryToSell(timestamp))))
+
+
+        val (newState2, _) = strategist.compute(newState, Event.SellingOrderConfirmed(timestamp, "id123"), timestamp + 100)
+        assert(newState2.cryptoCurrency == List(CryptoCurrencyInfo(RealNumber(200), 59940, CryptoCurrencyState.WaitingForSelling("id123", Nil))))
+
+        val (newState3, action2) = strategist.compute(newState2, Event.ReceiveOrderInfo("id123", List(Event.ConfirmOrderInfo("t1", RealNumber(50), 0, RealNumber(1)))), timestamp + 100)
+        assert(action2.contains(Action.RequestTradingInfo("id123", isBuying = false)))
+        assert(newState3.cryptoCurrency.contains(CryptoCurrencyInfo(RealNumber(150), 59940, CryptoCurrencyState.WaitingForSelling("id123", List("t1")))))
+
+        val (newState4, action3) = strategist.compute(newState3, Event.ReceiveOrderInfo("id123", List(Event.ConfirmOrderInfo("t2", RealNumber(150), 0, RealNumber(1)))), timestamp + 100)
+        assert(action3.contains(Action.RequestTradingInfo("id123", isBuying = false)))
+        assert(newState4.cryptoCurrency.isEmpty)
+    }
 }
