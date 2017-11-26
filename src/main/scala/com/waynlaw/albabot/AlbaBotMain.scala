f52cb0632e49d3f1b6f272a1add26f56c39acc9d
Wayne <waynejo1024@gmail.com>
2017-11-26 13:59:04 +0900

    update main function

--- src/main/scala/com/waynlaw/albabot/AlbaBotMain.scala
+++ src/main/scala/com/waynlaw/albabot/AlbaBotMain.scala
@@ -1,37 +1,26 @@
 package com.waynlaw.albabot
 
 import com.typesafe.scalalogging.LazyLogging
-import com.waynlaw.albabot.model.coin.CoinType
-import com.waynlaw.albabot.strategist.{RealWorld, Strategist}
-import com.waynlaw.albabot.strategist.model.StrategistModel
+import com.waynlaw.albabot.strategist.{DecisionMaker, RealWorld, Strategist}
+import com.waynlaw.albabot.strategist.model.{State, StrategistModel}
 import com.waynlaw.albabot.strategist.runner.Runner
-import com.waynlaw.albabot.util.{BithumbApi, HtmlUtil}
 
 object AlbaBotMain extends LazyLogging{
   def main(args: Array[String]) {
     Console println Configure.load()
 
     val realWorld = new RealWorld()
-    val strategist = new Strategist()
+    val decisionMaker = new DecisionMaker(coinUnitExp = -4)
+    val strategist = new Strategist(decisionMaker, 1000)
     val runner = new Runner(
-      StrategistModel(),
+      StrategistModel(
+        state = State.WaitingCurrencyInfo,
+        krw = 20000
+      ),
       strategist.compute,
       realWorld,
       realWorld
     )
-    //    runner.start()
-
-    //    Console println BithumbApi.getTicker("BCH")
-    //    Console println BithumbApi.getOrderbook("BCH")
-    //    Console println BithumbApi.getRecentTransactions("BCH")
-
-    //    Console println BithumbApi.tickerInfo("BTC")
-
-//      Console println BithumbApi.account(CoinType.BCH)
-//    logger.debug("here")
-//    Console println BithumbApi.place("QTUM", "0.1", "16000", "bid")
-//     orderId: String, `type`: String, count: String,  after: String
-//    Console println BithumbApi.orders("QTUM", "1511180986926412", "bid", "100", "1511211000")
-//    Console println BithumbApi.orderDetail("QTUM", "1511179593025365", "bid")
+    runner.start()
   }
 }
