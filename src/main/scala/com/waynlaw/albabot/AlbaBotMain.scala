27b76d42789e86616ad15b2654d9e9c613ea8366
Wayne <waynejo1024@gmail.com>
2017-12-04 21:27:14 +0900

    fix a main function

--- src/main/scala/com/waynlaw/albabot/AlbaBotMain.scala
+++ src/main/scala/com/waynlaw/albabot/AlbaBotMain.scala
@@ -3,17 +3,15 @@ package com.waynlaw.albabot
 import com.typesafe.scalalogging.LazyLogging
 import com.waynlaw.albabot.model.coin.CoinType
 import com.waynlaw.albabot.strategist.{DecisionMaker, RealWorld, Strategist}
-import com.waynlaw.albabot.strategist.model.{State, StrategistModel}
+import com.waynlaw.albabot.strategist.model._
 import com.waynlaw.albabot.strategist.runner.Runner
 
 object AlbaBotMain extends LazyLogging{
   def main(args: Array[String]) {
     Console println Configure.load()
 
-    val targetCoinType = CoinType.BTC
-
-    val realWorld = new RealWorld(targetCoinType)
-    val decisionMaker = new DecisionMaker(coinUnitExp = -4)
+    val realWorld = new RealWorld(CoinType.BTC)
+    val decisionMaker = new DecisionMaker(coinUnitExp = -3)
     val strategist = new Strategist(decisionMaker, 1000)
     val runner = new Runner(
       StrategistModel(),
