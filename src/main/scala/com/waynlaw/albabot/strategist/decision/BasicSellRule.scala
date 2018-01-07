a8b56d9db1509a0d1eb93d1c3d7a0dfdbfb74df8
Wayne <waynejo1024@gmail.com>
2018-01-08 03:27:59 +0900

    update selling rules.

--- src/main/scala/com/waynlaw/albabot/strategist/decision/BasicSellRule.scala
+++ src/main/scala/com/waynlaw/albabot/strategist/decision/BasicSellRule.scala
@@ -1,13 +1,29 @@
 package com.waynlaw.albabot.strategist.decision
 
-import com.waynlaw.albabot.strategist.model.{StrategistModel, TradeAction}
+import com.waynlaw.albabot.strategist.model.TradeAction.Sell
+import com.waynlaw.albabot.strategist.model.{CryptoCurrencyState, StrategistModel, TradeAction}
+import com.waynlaw.albabot.util.RealNumber
 
 case class BasicSellRule() extends TradeRule {
+    val MIN_PRICE_DIFF: BigInt = 100000
+
     override def update(state: StrategistModel, timestamp: BigInt): TradeRule = {
         BasicSellRule()
     }
 
     override def evaluate(state: StrategistModel, timestamp: BigInt): Option[TradeAction.TradeActionVal] = {
-        None
+        val historyAngle = DecisionUtil.historyAngle(state)
+        val lastPrice = DecisionUtil.lastPrice(state)
+        val sellCount = profitedSellCount(state, lastPrice, 3)
+        if (0 >= historyAngle && RealNumber(0) < sellCount) {
+            Some(Sell(sellCount, lastPrice))
+        } else {
+            None
+        }
+    }
+
+    private def profitedSellCount(state: StrategistModel, lastPrice: BigInt, coinUnitExp: Int): RealNumber = {
+        val sellItems = state.cryptoCurrency.filter(x => lastPrice - x.price >= MIN_PRICE_DIFF && x.state == CryptoCurrencyState.Nothing)
+        sellItems.map(_.amount).sum.floor(coinUnitExp)
     }
 }
